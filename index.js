469370c9e99e3609f2fe905a6bc9669ce027201b
Igor Silva <116300716+igorrsilvaaf@users.noreply.github.com>
2023-07-21 12:34:03 -0300

    Integraçao wpp

---
+++ index.js
@@ -0,0 +1,5 @@
+AOS.init();
+
+document.getElementById("whatsapp").addEventListener("click", function(){
+  window.location.href = "https://api.whatsapp.com/send?phone=5511000000000&text=Quero%20fazer%20um%20agendamento%20hoje!"
+})
