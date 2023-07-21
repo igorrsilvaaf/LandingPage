179b7b24b7261c4e90557a821f5e1ae64f4298bb
Igor Silva <116300716+igorrsilvaaf@users.noreply.github.com>
2023-07-21 13:18:34 -0300

    AjustesBotaoWpp

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// Animaçao da pagina
 AOS.init();
 
-document.getElementById("whatsapp").addEventListener("click", function(){
-  window.location.href = "https://api.whatsapp.com/send?phone=5511000000000&text=Quero%20fazer%20um%20agendamento%20hoje!"
-})
+//Evento do botao para abrir whatasapp
+const buttonsWpp = document.querySelectorAll("#whatsapp");
+
+buttonsWpp.forEach(button => {
+  button.addEventListener("click", function() {
+    window.location.href = "https://api.whatsapp.com/send?phone=5511000000000&text=Quero%20fazer%20um%20agendamento%20hoje!";
+  });
+});
