94707d4b09437bf7b0a6423645895c937bbe5ebd
Igor Silva <116300716+igorrsilvaaf@users.noreply.github.com>
2023-07-31 22:02:59 -0300

    ValidandoForm

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ buttonsWpp.forEach(button => {
     window.open("https://api.whatsapp.com/send?phone=5548991781573&text=Quero%20fazer%20um%20agendamento%20hoje!", "_blank");
   });
 });
+
+
+  // Função que irá mostrar a mensagem de sucesso no envio
+  function showSuccessMessage() {
+    document.getElementById("scheduleForm").style.display = "none";
+    document.getElementById("successMessage").style.display = "block";
+    document.querySelector(".schedule-description h2").innerText = "Mensagem enviada";
+  }
+
+  document.getElementById("scheduleForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    showSuccessMessage(); // Mostra a mensagem de sucesso
+  });
